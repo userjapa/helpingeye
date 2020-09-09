d7c9df917634138a0286e9b077c51802c2881654
usuarioJapa <willian.hsimabukuro@gmail.com>
2020-09-08 23:30:19 -0300

    setted camera to environment mode

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,21 @@
         canvas = document.getElementById('picture'),
         button = document.getElementById('capture')
 
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      width: {
+        min: 1280,
+        ideal: 1920,
+        max: 2560,
+      },
+      height: {
+        min: 720,
+        ideal: 1080,
+        max: 1440
+      },
+      facingMode: 'environment'
+    }
+  })
 
   video.srcObject = stream
 
