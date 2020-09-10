ff8b9f0c42cbf314bf613e5582891bda49fd4905
usuarioJapa <willian.hsimabukuro@gmail.com>
2020-09-10 15:41:16 -0300

    changed psm param

--- service/tesseract.js
+++ service/tesseract.js
@@ -3,7 +3,11 @@ const tesseract = require('node-tesseract-ocr');
 module.exports = {
   recognize (path, lang = 'por') {
     const options = {
-      lang
+      lang,
+      psm: 11,
+      env: {
+        maxBuffer: 4096 * 4096
+      }
     }
 
     return tesseract.recognize(path, options)
