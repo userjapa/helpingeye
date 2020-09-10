3eca10244a9ed29e0c50cf999a399dd781f2e847
usuarioJapa <willian.hsimabukuro@gmail.com>
2020-09-10 15:22:28 -0300

    added max width and height

--- service/tesseract.js
+++ service/tesseract.js
@@ -3,12 +3,8 @@ const tesseract = require('node-tesseract-ocr');
 module.exports = {
   recognize (path, lang = 'por') {
     const options = {
-      lang,
-      psm: 6,
-      env: {
-        maxBuffer: 4096 * 4096
-      }
-    };
+      lang
+    }
 
     return tesseract.recognize(path, options)
   }
